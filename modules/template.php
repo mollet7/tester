34ec4d93c2f9e5ad789e339603c641d2e01b01f0
mollet7 <mollet7@live.com>
2023-02-23 09:08:59 +0300

    php login and created templates

---
+++ modules/template.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP login Form </title>
+</head>
+<body>
+    <h1>How it made!</h1> <hr>
+    <div class="html">
+    <ul> <h2>HTML part</h2>
+       
+    </ul>
+</div>
+<div class="css">
+    <ul><h2>CSS Part</h2>
+        
+
+    </ul>
+</div>
+<div class="js">
+    <ul><h2>JS part</h2>
+        
+    </ul>
+</div>
+    <h2>Test Me</h2>
+    
+</body>
+</html>
+<style>
+    span {
+        background-color: darkgoldenrod;
+        color: white;
+        font-weight: bolder;
+        padding: 1px;
+        
+    }
+    ul {
+        padding: 10px;
+    }
+
+    ul li {
+       padding: 5px; 
+    }
+    .html {
+        border: 1px solid red;
+    }
+    .css {
+        border: 1px solid blue;
+    }
+
+    .js {
+        border: 1px solid green;
+    }
+
+</style>
+
+<script>
+   
+
+</script>
